8a7268bc1afff99aa893077b6e7df44e8ab76f26
rubenbch76 <rubenbch@gmail.com>
2022-01-11 18:47:07 +0100

    test finished. All OK

---
+++ DndCharacter.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * By adding type hints and enabling strict type checking, code can become
+ * easier to read, self-documenting and reduce the number of potential bugs.
+ * By default, type declarations are non-strict, which means they will attempt
+ * to change the original type to match the type specified by the
+ * type-declaration.
+ *
+ * In other words, if you pass a string to a function requiring a float,
+ * it will attempt to convert the string value to a float.
+ *
+ * To enable strict mode, a single declare directive must be placed at the top
+ * of the file.
+ * This means that the strictness of typing is configured on a per-file basis.
+ * This directive not only affects the type declarations of parameters, but also
+ * a function's return type.
+ *
+ * For more info review the Concept on strict type checking in the PHP track
+ * <link>.
+ *
+ * To disable strict typing, comment out the directive below.
+ */
+
+declare(strict_types=1);
+
+class DndCharacter
+{
+    public $strength;
+    public $dexterity;
+    public $constitution;
+    public $intelligence;
+    public $wisdom;
+    public $charisma;
+    public $hitpoints;
+
+    public function __construct()
+    {      
+        /* throw new BadFunctionCallException("Please implement the DndCharacter class!"); */
+    }
+
+    public static function modifier($num):int
+    {
+        $roundMethod = PHP_ROUND_HALF_DOWN;
+        
+        if ($num < 10) $roundMethod = PHP_ROUND_HALF_UP;
+
+        $abilityModifier = round(($num - 10) / 2, 0, $roundMethod);       
+        return intval($abilityModifier);
+    }
+
+    public static function ability():int
+    {
+        $results = array();
+
+        for ($i=1; $i<=4; $i++)
+        {
+            $num = rand(1, 6);
+            array_push($results, $num); 
+        }
+
+        $ability = array_sum($results) - min($results);
+        return $ability;
+    }
+    
+    public static function generate():object
+    {
+        $character = new self;
+
+        $character->strength = $character->ability();
+        $character->dexterity = $character->ability();
+        $character->constitution = $character->ability();
+        $character->intelligence = $character->ability();
+        $character->wisdom = $character->ability();
+        $character->charisma = $character->ability();
+
+        $character->hitpoints = 10 + $character->modifier($character->constitution);
+
+        return $character;
+    }
+}
